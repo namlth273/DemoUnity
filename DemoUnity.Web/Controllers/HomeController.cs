384232020ba405f3f3af7ddff1b39119695d94a9
Nam Le <nam.le@fmgsuite.com>
2019-12-20 18:29:09 +0700

    First commit

---
+++ DemoUnity.Web/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using DemoUnity.Web.Services;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace DemoUnity.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ITestService _testService;
+
+        public HomeController(ITestService testService)
+        {
+            _testService = testService;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            await _testService.HandleAsync();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DemoUnity.Web/Services/ITestService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DemoUnity.Web.Services
+{
+    public interface ITestService
+    {
+        Task HandleAsync();
+    }
+
+    public class TestService : ITestService
+    {
+        public Task HandleAsync()
+        {
+            Console.WriteLine("Hello");
+
+            return Task.CompletedTask;
+        }
+    }
+}
