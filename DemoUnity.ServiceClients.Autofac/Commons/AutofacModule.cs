9dc976f819e95e74bc4c56c9511757f33db4dab7
Nam Le <nam.le@fmgsuite.com>
2019-12-25 17:19:56 +0700

    - Add Autofac project.

---
+++ DemoUnity.ServiceClients.Autofac/Commons/AutofacModule.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using DemoUnity.ServiceClients.Abstractions.Services;
+using DemoUnity.ServiceClients.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DemoUnity.ServiceClients.Autofac.Commons
+{
+    public class AutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var services = new ServiceCollection();
+
+            services.AddHttpClient<ITestServiceClient, TestServiceClient>(client =>
+            {
+                client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com");
+            });
+
+            builder.Populate(services);
+        }
+    }
+}
